839ab835752e648c5f1055fe9d46b5d49ce8a6ca
Hadi <mhadimollataheri@gmail.com>
2024-05-19 17:46:37 +0330

    Install react-router-dom and Import needed functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<Root />}></Route>)
+);
+
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p className="bg-blue-300">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
