189456da769260606ae909e0d74d05ce945d6703
Hadi <mhadimollataheri@gmail.com>
2024-05-20 18:29:24 +0330

    Create element of Navber

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ShoppingCart } from '@phosphor-icons/react';
 
 function Navbar() {
-    return (<div>
-        Hi there
-    </div>);
+  return (
+    <div className=' h-16 text-white bg-black flex justify-end items-center pr-8'>
+      <button className='pr-2'>Shop</button>
+      <ShoppingCart size={24} color='#ffffff' />
+    </div>
+  );
 }
 
 export default Navbar;
