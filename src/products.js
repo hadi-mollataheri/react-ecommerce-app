067db273a403d082e0e6ad5096036bb1d85c2c77
Hadi <mhadimollataheri@gmail.com>
2024-05-21 11:04:39 +0330

    Create files and folders for assets images and pages -Create product.js file that contains the info of images

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import camera from './assets/camera.jpg';
+import iPhone from './assets/iPhone.jpg';
+import keyboard from './assets/keyboard.jpg';
+import laptop from './assets/laptop.jpg';
+import notebook from './assets/notebook.jpg';
+import sweater from './assets/sweater.jpg';
+import tShirt from './assets/t-shirt.jpg';
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productImage: camera,
+    productName: 'Camera',
+    price: '2100.0',
+  },
+  {
+    id: 2,
+    productImage: iPhone,
+    productName: 'iPhone',
+    price: '1000.0',
+  },
+  {
+    id: 3,
+    productImage: keyboard,
+    productName: 'Keyboard',
+    price: '100.0',
+  },
+  {
+    id: 4,
+    productImage: laptop,
+    productName: 'Laptop',
+    price: '1200.0',
+  },
+  {
+    id: 5,
+    productImage: notebook,
+    productName: 'Notebook',
+    price: '20.0',
+  },
+  {
+    id: 6,
+    productImage: sweater,
+    productName: 'Sweater',
+    price: '70.0',
+  },
+  {
+    id: 7,
+    productImage: tShirt,
+    productName: 'T Shirt',
+    price: '50.0',
+  },
+];
