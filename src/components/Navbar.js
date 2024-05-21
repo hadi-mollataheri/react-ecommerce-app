702a95fbdcc88e2c1a795a8fb01f0f88eeb1dbdd
Hadi <mhadimollataheri@gmail.com>
2024-05-21 14:40:18 +0330

    Re-write the Navbar component -Use Link component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ShoppingCart } from '@phosphor-icons/react';
 
 function Navbar() {
   return (
     <div className=' h-16 text-white bg-black flex justify-end items-center pr-8'>
-      <button className='pr-2'>Shop</button>
-      <ShoppingCart size={24} color='#ffffff' />
+      <Link to='/' className='mr-4 text-lg'>Shop</Link>
+      <Link to='/cart'><ShoppingCart size={32} color='#ffffff' /></Link>
     </div>
   );
 }
