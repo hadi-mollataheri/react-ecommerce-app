d440094b5b05866d340cc3b058d5cd4459f56257
Hadi <mhadimollataheri@gmail.com>
2024-05-22 17:11:53 +0330

    Add styles for mobile screens

--- src/pages/landing/Product.js
+++ src/pages/landing/Product.js
@@ -3,12 +3,14 @@ import React from 'react';
 function Product({ product }) {
   const { id, productImage, productName, price } = product;
   return (
-    <div key={id}>
-      <img src={productImage} alt={productName} />
-      <div id='info' className='flex flex-col items-center justify-center'>
-        <p className='mb-3 font-semibold'>{productName}</p>
-        <p className='mb-4'>{price}</p>
-        <button className=' border-2 border-black rounded-3xl px-0.5 pt-0.5 pb-1 m-0 box-border text-sm'>{`Add To Cart(${'num'})`}</button>
+    <div key={id} className='mt-12'>
+      <div className='max-w-full'>
+      <img src={productImage} alt={productName} className=' w-4/5 h-auto mx-auto block object-contain shadow-md' />
+      </div>
+      <div id='info' className='flex flex-col items-center justify-center mt-5'>
+        <p className='mb-2 font-semibold'>{productName}</p>
+        <p className='mb-3'>{price}</p>
+        <button className=' border-2 border-black rounded-3xl px-0.5 pt-0.5 pb-1 m-0 box-border text-xs'>{`Add To Cart(${'num'})`}</button>
       </div>
     </div>
   );
