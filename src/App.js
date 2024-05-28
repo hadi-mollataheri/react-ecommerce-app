4f18e6c8820516cc98ed4b356cbae2221a9104e7
Hadi <mhadimollataheri@gmail.com>
2024-05-28 16:16:33 +0330

    Update Shop link in Navbar.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route
-            path='/react-ecommerce-app'
+            path='/'
             element={
               <ShopContextProvider>
                 <Landing />
